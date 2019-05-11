c8a307ce0fea1b6363395d58a6a156b711eb4c37
Napwob <gorno-altaisk0@mail.ru>
2019-05-11 17:33:05 +0300

    Menu Version A.1

--- src/main.c
+++ src/main.c
@@ -4,18 +4,24 @@
 #include <time.h>
 #include <stdio.h>
 #include <dos.h>
-#define UP_ARROW 72
-#define LEFT_ARROW 75
-#define DOWN_ARROW 80
-#define RIGHT_ARROW 77
+#define UP_ARROW 1
+#define LEFT_ARROW 2
+#define DOWN_ARROW 3
+#define RIGHT_ARROW 4
+#define VK_NUMPAD1 5
+#define VK_NUMPAD2 6
 #include <locale.h>
 
 void drawmenu(){
 printf("1 - Game\n2 - Exit\n");
-
 }
 
 int main()
 {
-
+    while(1){
+    int n,i;
+    drawmenu();
+    n=getch();
+    if(n=6)return 0;
+    }
 }
