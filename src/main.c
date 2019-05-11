6570b473f9d550623800b931027b7ba3f6b82e6b
Napwob <gorno-altaisk0@mail.ru>
2019-05-11 17:04:36 +0300

    Create main file

---
+++ src/main.c
@@ -0,0 +1,21 @@
+#include <windows.h>
+
+#include <conio.h>
+#include <time.h>
+#include <stdio.h>
+#include <dos.h>
+#define UP_ARROW 72
+#define LEFT_ARROW 75
+#define DOWN_ARROW 80
+#define RIGHT_ARROW 77
+#include <locale.h>
+
+void drawmenu(){
+printf("1 - Game\n2 - Exit\n");
+
+}
+
+int main()
+{
+
+}
