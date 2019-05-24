3859abc02f3612cfd8d2420fa4d3c6ce74029a6c
conner <kadconner@mail.ru>
2019-05-24 17:20:32 +0700

    Vivod cvetnogo menu(v 1.1)

--- src/output_matrix.c
+++ src/output_matrix.c
@@ -1,10 +1,7 @@
+#include <ncurses.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define UP_ARROW 1
-#define LEFT_ARROW 2
-#define DOWN_ARROW 3
-#define RIGHT_ARROW 4
 int x[4][4];
 void matrix()
 {
@@ -18,13 +15,25 @@ void matrix()
 }
 void output()
 {
+    int x1 = 50, y1 = 10;
+    initscr();
+    start_color();
+    init_pair(1, COLOR_RED, COLOR_BLACK);
+    attron(A_BOLD);
+    attron(COLOR_PAIR(1));
     for (int i = 0; i < 4; i++) {
+        x1 = 50;
         for (int j = 0; j < 4; j++) {
-            printf("|");
-            printf("%4d  ", x[i][j]);
+            move(y1, x1);
+            printw("|");
+            printw("%4d  ", x[i][j]);
+            x1 = x1 + 10;
         }
-        printf("|\n");
+        printw("|\n\n");
+        y1 = y1 + 5;
     }
+    getch();
+    endwin();
 }
 
 int main()
