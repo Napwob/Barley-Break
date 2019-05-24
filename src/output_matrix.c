622a289677e067bfaab7a593974c74e7d8a6d642
conner <kadconner@mail.ru>
2019-05-24 18:28:59 +0700

    Krasivie granici v matrice

--- src/output_matrix.c
+++ src/output_matrix.c
@@ -16,21 +16,31 @@ void matrix()
 void output()
 {
     int x1 = 50, y1 = 10;
+    int x2 = 50, y2 = 9;
+    int x3 = 50, y3 = 11;
     initscr();
     start_color();
-    init_pair(1, COLOR_RED, COLOR_BLACK);
+    init_pair(1, COLOR_GREEN, COLOR_BLACK);
     attron(A_BOLD);
     attron(COLOR_PAIR(1));
     for (int i = 0; i < 4; i++) {
         x1 = 50;
+        x2 = 50;
+        x3 = 50;
         for (int j = 0; j < 4; j++) {
+            move(y2, x2);
+            printw("+===+");
             move(y1, x1);
-            printw("|");
-            printw("%4d  ", x[i][j]);
+            printw("|%2d |", x[i][j]);
+            move(y3, x3);
+            printw("+===+");
             x1 = x1 + 10;
+            x2 = x2 + 10;
+            x3 = x3 + 10;
         }
-        printw("|\n\n");
         y1 = y1 + 5;
+        y2 = y2 + 5;
+        y3 = y3 + 5;
     }
     getch();
     endwin();
