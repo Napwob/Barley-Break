b31d15a522f8577e1d35c1cc709fc0d28a7209c6
conner <kadconner@mail.ru>
2019-05-30 16:33:51 +0700

    iGahu change

--- src/gafu.h
+++ src/gafu.h
@@ -1,11 +1,8 @@
 #ifndef GAFU_H
 #define GAFU_H
-#include <conio.h>
-#include <dos.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <windows.h>
 
 struct c fit(int* numb[4][4], struct c c);
 int wich(int n[4][4]);
