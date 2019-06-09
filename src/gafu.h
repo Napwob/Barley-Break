9ffe8e175bdbc27ff1c1c19961dad970f5947f13
Napwob <gorno-altaisk0@mail.ru>
2019-06-09 14:43:45 +0300

    Merge Error Fix

--- src/gafu.h
+++ src/gafu.h
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-||||||| merged common ancestors
-#ifndef GAFU_H
-#define GAFU_H
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-struct c fit(int* numb[4][4], struct c c);
-int wich(int n[4][4]);
-void game();
-
-#endif
-=======
 #ifndef GAFU_H
 #define GAFU_H
 #include <ncurses.h>
@@ -25,4 +11,4 @@ struct c moveit(int numb[][4], struct c c,int pr)
 int game();
 
 #endif
->>>>>>> Napwob
+
